b0dc67b0eecf6e0b1d4ef244090c708f7c974281
Jason Berney <jasonrberney@gmail.com>
2020-01-28 17:06:04 -0800

    2.3-2.7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+// can also use template and type html there instead of templateURL
+// same with styles or styleUrls
+// Decorators come with metadata options such as selector, etc.
 @Component({
-    selector: 'app-root',
-    template: '<h1>Hi Jason</h1>'
+    selector: 'mw-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {}
