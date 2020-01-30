42157b3b0f647cd6dd954bc3c2d8e1d80049da23
Jason Berney <jasonrberney@gmail.com>
2020-01-29 16:27:33 -0800

    2.08-2.13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,16 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {}
+export class AppComponent {
+    firstMediaItem = {
+        id: 1,
+        name: 'Firebug',
+        medium: 'Series',
+        category: 'Science Fiction',
+        year: 2010,
+        watchedOn: 1294166565384,
+        isFavorite: false
+    }
+
+    onMediaItemDelete(mediaItem) {}
+}
--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+// everything defined in component is made available in the template (no 'this' or anything)
+export class MediaItemComponent {
+    // Input can take an alias which can be referenced in the html markup and it will still be considered/mapped 'mediaItem'
+    @Input() mediaItem;
+    @Output() delete = new EventEmitter();
+
+    onDelete() {
+        console.log('delete')
+        this.delete.emit(this.mediaItem);
+    }
+}
