d43861b9f7c737273c9b1c422e787960819333e6
Jason Berney <jasonrberney@gmail.com>
2020-01-29 17:07:03 -0800

    3.01-3.06

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
 
 @NgModule({
     imports: [
@@ -9,7 +12,10 @@ import { MediaItemComponent } from './media-item.component';
     ],
     declarations: [
         AppComponent,
-        MediaItemComponent
+        MediaItemComponent,
+        MediaItemListComponent,
+        FavoriteDirective,
+        CategoryListPipe
     ],
     bootstrap: [
         AppComponent
